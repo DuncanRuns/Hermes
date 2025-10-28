7383967e7694313762c7688f9f27ec7e26dd1ee6
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2025-10-28 09:38:45 -0300

    Include positions in dimension changes

--- src/main/java/me/duncanruns/hermes/playlog/DimensionTracker.java
+++ src/main/java/me/duncanruns/hermes/playlog/DimensionTracker.java
@@ -2,31 +2,40 @@ package me.duncanruns.hermes.playlog;
 
 import com.google.gson.JsonObject;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.Vec3d;
 
 import java.util.*;
 
 // Implementation note: Doing a mixin to changeDimension is just not a good idea, as it isn't called on respawns, and
 // sometimes gets spammed when exiting the end. Doing a tracker like this resolves all of those issues and is easier.
 public class DimensionTracker {
-    Map<UUID, String> dimensions = new HashMap<>();
+    Map<UUID, String> oldDimensions = new HashMap<>();
+    Map<UUID, Vec3d> oldPositions = new HashMap<>();
 
     /**
      * @return A list of json objects representing the changes.
      */
     public Iterable<JsonObject> tick(MinecraftServer minecraftServer) {
         // Remove players that have left to prevent minor leakage, and mirrors the behavior of a solo player relogging for non host players.
-        dimensions.keySet().removeIf(uuid -> minecraftServer.getPlayerManager().getPlayer(uuid) == null);
+        oldDimensions.keySet().removeIf(uuid -> minecraftServer.getPlayerManager().getPlayer(uuid) == null);
+        oldPositions.keySet().removeIf(uuid -> minecraftServer.getPlayerManager().getPlayer(uuid) == null);
         List<JsonObject> changes = new ArrayList<>();
         minecraftServer.getPlayerManager().getPlayerList().forEach(player -> {
             UUID id = player.getGameProfile().getId();
-            String oldDimension = dimensions.getOrDefault(id, null);
+
+            Vec3d newPos = player.getPos();
+            Vec3d oldPos = oldPositions.put(id, newPos);
+
             String newDimension = player.world.getRegistryKey().getValue().toString();
+            String oldDimension = oldDimensions.put(id, newDimension);
             if (Objects.equals(oldDimension, newDimension)) return;
-            dimensions.put(id, newDimension);
 
             JsonObject data = new JsonObject();
             data.add("player", PlayLog.toPlayerData(player));
+            data.addProperty("old_dimension", oldDimension);
+            data.add("old_position", oldPos == null ? null : PlayLog.toPositionData(oldPos));
             data.addProperty("dimension", newDimension);
+            data.add("position", PlayLog.toPositionData(newPos));
             changes.add(data);
         });
         return changes;
