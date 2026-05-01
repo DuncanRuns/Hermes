c00fa87f4e8e4fbb8247bd0acfdff3a9608c113e
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2026-04-30 22:01:50 -0300

    this wasn't very useful

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,37 +12,31 @@ plugins {
     id("dev.kikugie.stonecutter") version "0.9.2"
 }
 
-val developSingleVersion = null
-
 stonecutter {
     create(rootProject) {
         // See https://stonecutter.kikugie.dev/wiki/start/#choosing-minecraft-versions
-        if(developSingleVersion == null) {
-            versions(
-                "1.14.3",
-                "1.14.4",
-                "1.15",
-                "1.15.1",
-                "1.15.2",
-                "1.16",
-                "1.16.1",
-                "1.16.2",
-                "1.16.3",
-                "1.16.4",
-                "1.16.5",
-                "1.17",
-                "1.17.1",
-                "1.18",
-                "1.18.1",
-                "1.18.2",
-                "1.19",
-                "1.19.1",
-                "1.19.2",
-                )
-            vcsVersion = "1.16.1"
-        } else {
-            version(developSingleVersion)
-        }
+        versions(
+            "1.14.3",
+            "1.14.4",
+            "1.15",
+            "1.15.1",
+            "1.15.2",
+            "1.16",
+            "1.16.1",
+            "1.16.2",
+            "1.16.3",
+            "1.16.4",
+            "1.16.5",
+            "1.17",
+            "1.17.1",
+            "1.18",
+            "1.18.1",
+            "1.18.2",
+            "1.19",
+            "1.19.1",
+            "1.19.2",
+        )
+        vcsVersion = "1.16.1"
     }
 }
 
